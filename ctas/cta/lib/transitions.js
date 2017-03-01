c1b0bca14cd815481c2b69a23415a33bf23657e0
Dallas Read <dallas@excitecreative.ca>
2017-02-28 22:12:28 -0400

    Zepto-able transitions.

--- ctas/cta/lib/transitions.js
+++ ctas/cta/lib/transitions.js
@@ -16,21 +16,41 @@ module.exports = {
 
     fadeIn: function fadeIn() {
         var _ = this;
-        _.$el.fadeIn();
+
+        if (typeof _.$el.fadeIn === 'function') {
+            _.$el.fadeIn();
+        } else {
+            _.$el.show();
+        }
     },
 
     fadeOut: function fadeOut() {
         var _ = this;
-        _.$el.fadeOut();
+
+        if (typeof _.$el.fadeOut === 'function') {
+            _.$el.fadeOut();
+        } else {
+            _.$el.hide();
+        }
     },
 
     fadeToggle: function fadeToggle() {
         var _ = this;
-        _.$el.fadeToggle();
+
+        if (typeof _.$el.fadeToggle === 'function') {
+            _.$el.fadeToggle();
+        } else {
+            _.$el.toggle();
+        }
     },
 
     slideToggle: function slideToggle() {
         var _ = this;
-        _.$el.slideToggle();
-    },
+
+        if (typeof _.$el.slideToggle === 'function') {
+            _.$el.slideToggle();
+        } else {
+            _.$el.toggle();
+        }
+    }
 };
