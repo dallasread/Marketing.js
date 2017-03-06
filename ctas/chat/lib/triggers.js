0d16aa8dae01757115d597213d945d11a7d0ae09
Dallas Read <dallas@excitecreative.ca>
2017-03-05 22:12:06 -0400

    Tweak triggers.

--- ctas/chat/lib/triggers.js
+++ ctas/chat/lib/triggers.js
@@ -4,32 +4,36 @@
 
 var Trigger = require('../../cta/lib/trigger');
 
+function sendMsg(cta, message) {
+    var thing = {
+            model: 'event',
+            data: {
+                action: 'message',
+                from: 'agent',
+                cta: cta.get('cta.id'),
+                user: cta.get('user.id'),
+                agent: cta.get('agent.id'),
+                message: message
+            }
+        };
+
+    cta.api.post('/things', {
+        thing: thing
+    });
+
+    cta.addMessage(thing);
+}
+
 Trigger.registerEvent('noMessages', function bindNoMessageEvent() {
     var _ = this;
 
     _.cta.on(_.event, function noMessageEvent() {
         _.trigger(function() {
-            _.cta.addMessage({
-                data: {
-                    action: 'message',
-                    from: 'agent',
-                    agent: _.cta.get('agent'),
-                    message: _.message
-                }
-            });
+            sendMsg(_.cta, _.message);
         });
     });
 });
 
 Trigger.registerAction('sendMessage', function sendMessage() {
-    var _ = this;
-
-    _.cta.addMessage({
-        data: {
-            action: 'message',
-            from: 'agent',
-            agent: _.cta.get('agent'),
-            message: _.message
-        }
-    });
+    sendMsg(_.cta, _.message);
 });
